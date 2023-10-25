eaa9223d0ee97c85ee93d786bb0321e365aeac06
Casey McMahon <mcmahon.casey@outlook.com>
2023-10-24 18:08:40 -0600

    - pingpong is now a combined tx/rx test - fully functional for ssh based tests - TODO: print messages to bonnet OLED

--- examples/pingpong.rs
+++ examples/pingpong.rs
@@ -1,6 +1,7 @@
 use clap::{Args, Parser, Subcommand};
-use std::error::Error;
-use std::time::Duration;
+use std::{error::Error, thread::sleep};
+use std::time::{Duration, Instant};
+use chrono::prelude::*;
 
 use rfm9x::{Band, Channel, DataRate, RFM95};
 use rppal::spi::{Bus, Mode, SlaveSelect, Spi};
@@ -29,20 +30,28 @@ struct PingArgs {
 
     /// the time in seconds between pings. If set to zero, it will only ping when a pong has been recieved (except the first one).
     #[arg(short, long, default_value_t = 0)]
-    delay: u32,
+    delay: u64,
+
+    /// The amount of time the program will listen for a ping. Resets after each recieved ping.
+    #[arg(short, long, default_value_t = 120)]
+    timeout: u64,
 }
 
 #[derive(Args)]
 struct PongArgs {
     #[arg(short, long, default_value_t = 120)]
     /// The amount of time the program will listen for a ping. Resets after each recieved ping.
-    time: u32,
+    timeout: u64,
 }
 
-fn ping(count: u8, delay: u32) {
-    let spi: rppal::spi::Spi =
-        Spi::new(Bus::Spi0, SlaveSelect::Ss1, 4_000_000, Mode::Mode0).unwrap();
 
+
+fn setup_radio() -> Result<RFM95, Box<dyn Error>> {
+    // Define spi
+    let spi: rppal::spi::Spi =
+        Spi::new(Bus::Spi0, SlaveSelect::Ss1, 4_000_000, Mode::Mode0)?;
+
+    // Define radio
     let mut rfm = RFM95::new(
         spi,
         22,
@@ -51,16 +60,87 @@ fn ping(count: u8, delay: u32) {
         Band::US901,
         Channel::Ch3,
     )
-    .unwrap();
+    ?;
 
-    rfm.reset(25).unwrap();
-    let message = "PING";
+    rfm.reset(25)?;
 
-    rfm.send_packet(message.as_bytes()).unwrap(); // Raw packet
+    // recieve a packet (unused, not sure why but it needs to happen.)
+    let (_pkt, _size) = rfm.receive_packet(
+        Channel::Ch3,
+        DataRate::SF7_BW125,
+        false,
+        Duration::from_secs(1),
+    ).unwrap();
+    
+    return Ok(rfm);
 }
 
-fn pong(timeout: u32) {
-    unimplemented!()
+
+fn send_it(rfm: &mut RFM95, m: &str) -> Result<(), Box<dyn Error>> {
+
+    let msg = format!("TX: [{}] - {}", Utc::now().to_string(), m);
+    println!("{}", msg);
+    rfm.send_packet(msg.as_bytes()).unwrap();
+    Ok(())
+}
+
+fn get_it(rfm: &mut RFM95, timeout: u64) -> Result<String, Box<dyn Error>> {
+    let (pkt, _size) = rfm.receive_packet(
+        Channel::Ch3,
+        DataRate::SF7_BW125,
+        false,
+        Duration::from_secs(timeout + 1),
+    ).unwrap();
+
+    let msg = String::from_utf8_lossy(&pkt);
+    let msg2 = msg.strip_suffix(0 as char).unwrap();
+
+    return Ok(format!("RX: [{}] [RSSI: {}] [SNR: {}]- {}", Utc::now().to_string(), rfm.get_rssi().unwrap(),rfm.get_rssi().unwrap(), msg2));
+}
+
+fn ping(count: u8, delay: u64, timeout: u64) -> Result<(), Box<dyn Error>> {
+    
+    // setup radio
+    let mut rfm = setup_radio().unwrap();
+
+    // send first packet
+    send_it(&mut rfm, "PING")?;
+
+    let mut counter: u8 = 1;
+    let mut now = Instant::now();
+    while counter < count {
+        let m = get_it(&mut rfm, timeout)?;
+        if now.elapsed().as_secs() >= timeout.into() {
+            println!("\nTIMEOUT\n");
+            break;
+        }
+        println!("{}", m);
+        sleep(Duration::from_secs(delay));
+        send_it(&mut rfm, "PING")?;
+        now = Instant::now();
+        counter += 1;
+    }
+
+    Ok(())
+}
+
+fn pong(timeout: u64) -> Result<(), Box<dyn Error>> {
+    
+    let mut rfm = setup_radio()?;
+
+    let mut now = Instant::now();
+    loop {
+        let m = get_it(&mut rfm, timeout)?;
+        if now.elapsed().as_secs() >= timeout.into() {
+            println!("\nTIMEOUT\n");
+            break;
+        }
+        println!("{}", m);
+        send_it(&mut rfm, "PONG")?;
+        now = Instant::now();
+    }
+
+    Ok(())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -69,11 +149,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     // You can check for the existence of subcommands, and if found use their
     // matches just as you would the top level cmd
     match &cli.command {
-        Commands::Ping(ping) => {
-            unimplemented!()
+        Commands::Ping(args) => {
+            ping(args.count, args.delay, args.timeout)?;
         }
-        Commands::Pong(pong) => {
-            unimplemented!()
+        Commands::Pong(args) => {
+            pong(args.timeout)?;
         }
     }
+
+    Ok(())
 }
